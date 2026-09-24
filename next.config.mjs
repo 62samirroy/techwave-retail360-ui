181a017c0f4acd4d300b4a1a2565b35e65b50204
Samir Roy <20203024.it@gmail.com>
2026-09-25 02:36:52 +0530

    fix(ui): add pexels domains and remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    domains: [
+      'images.unsplash.com',
+      'plus.unsplash.com',
+      'images.pexels.com',
+      'pexels.com',
+    ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'plus.unsplash.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.pexels.com',
+        pathname: '/**',
       },
     ],
   },
